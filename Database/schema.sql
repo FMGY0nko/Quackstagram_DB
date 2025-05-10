b499524ca06a844c67b1e659e4cfcb049f8e3abe
FMGY0nko <theevilrabbit999@gmail.com>
2025-05-10 03:40:29 +0200

    updated schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,6 +1,6 @@
 -- Used to store user information
 CREATE TABLE user (
-    ID INT PRIMARY KEY,
+    ID VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     bio TEXT DEFAULT "No bio",
     profile_picture VARCHAR(255) DEFAULT "default_profile.png"
@@ -8,9 +8,8 @@ CREATE TABLE user (
 
 -- Used to store user-uploaded pictures
 CREATE TABLE uploaded_picture (
-    uploading_user INT NOT NULL,
+    uploading_user VARCHAR(255) NOT NULL,
     number INT NOT NULL,
-    path VARCHAR(255) NOT NULL,
     bio TEXT,
     time_uploaded DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (uploading_user, number),
@@ -19,9 +18,10 @@ CREATE TABLE uploaded_picture (
 
 -- Used to track likes on pictures
 CREATE TABLE picture_like (
-    liking_user INT NOT NULL,
-    uploading_user INT NOT NULL,
+    liking_user VARCHAR(255) NOT NULL,
+    uploading_user VARCHAR(255) NOT NULL,
     picture_number INT NOT NULL,
+    number INT NOT NULL NULL PRIMARY KEY,
     time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (liking_user) REFERENCES user(ID) ON DELETE CASCADE,
     FOREIGN KEY (uploading_user, picture_number) REFERENCES uploaded_picture(uploading_user, number) ON DELETE CASCADE
@@ -29,8 +29,8 @@ CREATE TABLE picture_like (
 
 -- Used to track follow relationships
 CREATE TABLE user_follow (
-    user_following INT NOT NULL,
-    user_followed INT NOT NULL,
+    user_following VARCHAR(255) NOT NULL,
+    user_followed VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_following) REFERENCES user(ID) ON DELETE CASCADE,
     FOREIGN KEY (user_followed) REFERENCES user(ID) ON DELETE CASCADE,
     CHECK (user_following != user_followed)
@@ -38,7 +38,7 @@ CREATE TABLE user_follow (
 
 -- Used to track logged-in users
 CREATE TABLE current_user (
-    ID INT PRIMARY KEY,
+    ID VARCHAR(255) PRIMARY KEY,
     FOREIGN KEY (ID) REFERENCES user(ID) ON DELETE CASCADE
 );
 
@@ -49,7 +49,7 @@ INSERT INTO user (ID, password, bio, profile_picture) VALUES
 (2, "anotherpass", "Big fan of the miami dolphins", "tyreek_hill.png"),
 (3, "paswordexample", "Some random example text", DEFAULT);
 
--- upload_picture example statements
+-- uploaded_picture example statements
 -- need to ask for clarification for the number
 
 -- picture_like example statements
